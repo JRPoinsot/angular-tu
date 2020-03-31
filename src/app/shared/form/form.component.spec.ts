a5fd63d0a5c80e8ad62706e1b38bbf02c4a3c038
Jean-Rodolphe POINSOT <jr.poinsot@gmail.com>
2020-03-31 18:58:58 +0200

    Review FormComponent test

--- src/app/shared/form/form.component.spec.ts
+++ src/app/shared/form/form.component.spec.ts
@@ -1,15 +1,12 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {DebugElement} from '@angular/core/src/debug/debug_node';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {DebugElement, NO_ERRORS_SCHEMA} from '@angular/core';
 import { FormComponent } from 'app/shared/form/form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatInputModule
-} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 const fakePerson = {
     id: '5763cd4d9d2a4f259b53c901',
